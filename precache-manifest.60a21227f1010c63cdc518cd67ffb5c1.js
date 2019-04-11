cb1addddc563ac671f6e2b0e721585bcd6ea52e6
JaroslawLis <niejarek@poczta.onet.pl>
2019-04-11 21:47:11 +0200

    Updates

---
+++ precache-manifest.60a21227f1010c63cdc518cd67ffb5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac54d42ce022ab979abe",
+    "url": "/job-offers-list-react-app/static/css/main.28c0793d.chunk.css"
+  },
+  {
+    "revision": "ac54d42ce022ab979abe",
+    "url": "/job-offers-list-react-app/static/js/main.44374958.chunk.js"
+  },
+  {
+    "revision": "d05fce8916c94726e25f",
+    "url": "/job-offers-list-react-app/static/js/runtime~main.9eedf917.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/job-offers-list-react-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/job-offers-list-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "36ddc10999bb149dc6b767d8b81930d2",
+    "url": "/job-offers-list-react-app/index.html"
+  }
+];
