1484a03b6446490ed699701fa47977a08f64aae9
JaroslawLis <niejarek@poczta.onet.pl>
2019-04-17 22:31:48 +0200

    Updates

--- precache-manifest.58749d33e1c8560949ac29dc3be09298.js
+++ precache-manifest.58749d33e1c8560949ac29dc3be09298.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
-    "revision": "d4fcee44097d0c3e5fdd",
+    "revision": "294b5884739e7583d422",
     "url": "/job-offers-list-react-app/static/css/main.d0ad0f8d.chunk.css"
   },
   {
-    "revision": "d4fcee44097d0c3e5fdd",
-    "url": "/job-offers-list-react-app/static/js/main.207743a6.chunk.js"
+    "revision": "294b5884739e7583d422",
+    "url": "/job-offers-list-react-app/static/js/main.8cf8cca5.chunk.js"
   },
   {
     "revision": "d05fce8916c94726e25f",
     "url": "/job-offers-list-react-app/static/js/runtime~main.9eedf917.js"
   },
   {
-    "revision": "7540c4b8a3285aabda4d",
-    "url": "/job-offers-list-react-app/static/js/2.e37e8b1b.chunk.js"
+    "revision": "219635517f5a5e3c9873",
+    "url": "/job-offers-list-react-app/static/js/2.bc5fbade.chunk.js"
   },
   {
-    "revision": "42f359c5d0741df8c0125e1677992630",
+    "revision": "3516459e61248cafedbb9a683f5e2528",
     "url": "/job-offers-list-react-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/job-offers-list-react-app/precache-manifest.9713483a59ddbc3cd0fd02073672501d.js"
+  "/job-offers-list-react-app/precache-manifest.58749d33e1c8560949ac29dc3be09298.js"
 );
 
 workbox.clientsClaim();
